ead5d0239a612c61bf3d09d297175e96842b1262
justinemoulin <justinemoulin5@icloud.com>
2022-11-19 17:15:35 +0100

    Ajout classe DialogueNiveaux

---
+++ Sokoban/src/Sokoban/DialogueNiveaux.java
@@ -0,0 +1,61 @@
+package Sokoban;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextPane;
+
+public class DialogueNiveaux extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			DialogueNiveaux dialog = new DialogueNiveaux();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public DialogueNiveaux() {
+		setBounds(100, 100, 250, 200);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setLayout(new FlowLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		{
+			JTextPane txtNiveauSuivant = new JTextPane();
+			txtNiveauSuivant.setText("Bravo !\nTu as réussi !\nVeux-tu passer au niveau suivant ?");
+			contentPanel.add(txtNiveauSuivant);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Quitter");
+				cancelButton.setActionCommand("Quitter");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+
+}
