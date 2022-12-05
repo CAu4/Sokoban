397a9be757bbbee896e7c4062ce51e2995d56c90
justinemoulin <justinemoulin5@icloud.com>
2022-12-05 10:23:59 +0100

    Test connexion base

--- Sokoban/src/Sokoban/prog_princ.java
+++ Sokoban/src/Sokoban/prog_princ.java
@@ -1,6 +1,7 @@
 package Sokoban;
 
 import javax.swing.ImageIcon;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.EventQueue;
@@ -28,28 +29,6 @@ public class prog_princ {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		/*
-		 * public static void main(String[] args) { try { // create our mysql database
-		 * connection String myDriver = "com.mysql.cj.jdbc.Driver"; String myUrl =
-		 * "jdbc:mysql://localhost:8889/desamiantageMazet"; Class.forName(myDriver);
-		 * Connection conn = DriverManager.getConnection(myUrl, "root", "root");
-		 * 
-		 * // our SQL SELECT query. // if you only need a few columns, specify them by
-		 * name instead of using "*" String query = "SELECT * FROM poste";
-		 * 
-		 * // create the java statement Statement st = conn.createStatement();
-		 * 
-		 * // execute the query, and get a java resultset ResultSet rs =
-		 * st.executeQuery(query);
-		 * 
-		 * // iterate through the java resultset while (rs.next()) { int id =
-		 * rs.getInt("IdPoste"); String intitule = rs.getString("IntitulePoste");
-		 * System.out.format("%s, %s\n", id, intitule); } st.close(); } catch (Exception
-		 * e) { System.err.println("Got an exception! ");
-		 * System.err.println(e.getMessage()); } }
-		 */
-		
 	}
 
 }
